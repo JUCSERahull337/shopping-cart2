d3c8e4e9eebe2b0f816bd8953bc2414003eb7166
JUCSERahull337 <45335164+JUCSERahull337@users.noreply.github.com>
2021-04-20 22:42:35 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+// console.log('hello')
+
+let AddCart= document.querySelectorAll('.add-cart');
+
+for (let i = 0; i < AddCart.length; i++) {
+    
+    AddCart[i].addEventListener('click',(event) =>{
+        //console.log('clicked');
+        cartNumber();
+        event.preventDefault();
+    })
+    
+}
+
+function onLoadPage(){
+    let productNumber= localStorage.getItem('cartNumber')
+
+    if(productNumber){
+        document.querySelector('.cart span').textContent=0;
+
+    }
+
+}
+
+function cartNumber(){
+    let productNumber= localStorage.getItem('cartNumber')
+    productNumber= parseInt(productNumber)
+    // console.log(productNumber)
+
+    if(productNumber){
+        localStorage.setItem('cartNumber',productNumber+1)
+        document.querySelector('.cart span').textContent=productNumber+1;
+
+    }
+    else{
+        localStorage.setItem('cartNumber',1)
+        document.querySelector('.cart span').textContent=1;
+    }
+  
+   
+}
+onLoadPage();
